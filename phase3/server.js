ddd5c7deddd8a8410bc7ef0e5cc4998d0b9e3d6a
prach19 <prachi19@my.yorku.ca>
2022-12-04 20:35:40 -0500

    fixed server side (added public files to folder)

--- phase3/server.js
+++ phase3/server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get(["/", "/index.html"], (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// })
 
 
 // start the server
